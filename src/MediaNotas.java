3ca8ca206722a951de0f803dc2facf27ecbd84e0
oqvine <oqvine@live.com>
2024-11-21 16:21:21 -0300

    Commit para incluir calculo entre variaveis numericas

---
+++ src/MediaNotas.java
@@ -0,0 +1,21 @@
+public class MediaNotas {
+
+    public static void main (String args[]) {
+        calculoMedia();
+    }
+
+    private static void calculoMedia() {
+        int media1 = 6;
+
+        int media2 = 7;
+
+        int media3 = 4;
+
+        int media4 = 9;
+
+        int calculoMedia = (media1 + media2 + media3 + media4)/4;
+
+        System.out.println("A média total é " + calculoMedia);
+    }
+
+}
